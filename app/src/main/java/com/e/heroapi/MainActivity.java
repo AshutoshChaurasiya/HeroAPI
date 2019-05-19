d2289209c60e67f4a34156146ed1e613e863a9d9
Softwarica College Lab <softwarica@softwarica.com>
2019-05-19 11:29:38 +0545

    Initial commit

---
+++ app/src/main/java/com/e/heroapi/MainActivity.java
@@ -0,0 +1,73 @@
+package com.e.heroapi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import heroesapi.HeroesAPI;
+import model.Heroes;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import url.Url;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText etName, etDescription;
+    private Button btnSave;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etName = findViewById(R.id.etname);
+        etDescription = findViewById(R.id.etDesc);
+        btnSave = findViewById(R.id.btnSave);
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Save();
+            }
+
+            private void Save() {
+            }
+
+//            private void Save() {
+//                String name = etName.getText().toString();
+//                String desc = etDescription.getText().toString();
+//
+//                Heroes heroes = new Heroes(name, desc);
+//                Retrofit retrofit = new Retrofit.Builder()
+//                        .baseUrl(Url.BASE_URL)
+//                        .addConverterFactory(GsonConverterFactory.create())
+//                        .build();
+//
+//                HeroesAPI heroesAPI = retrofit.create(HeroesAPI.class);
+//
+//                Call<Void> heroesCall = heroesAPI.addHero(heroes);
+//
+//                heroesCall.enqueue(new Callback<Void>() {
+//                    @Override
+//                    public void onResponse(Call<Void> call, Response<Void> response) {
+//                        if (!response.isSuccessful()) {
+//                           Toast.makeText(MainActivity.this, " code " + response.code(), Toast.LENGTH_SHORT).show();
+//                        }
+//                        Toast.makeText(MainActivity.this, "Sucessfully Added", Toast.LENGTH_SHORT).show();
+//                    }
+//
+//                    @Override
+//                    public void onFailure(Call<Void> call, Throwable t) {
+//                        Toast.makeText(MainActivity.this, "Error" + t.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+//                    }
+//                });
+//
+//            }
+        });
+    }
+}
