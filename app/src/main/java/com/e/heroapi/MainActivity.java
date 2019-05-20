54e4e18e3d2edacee8bd247e06c68dedce87e683
Softwarica College Lab <softwarica@softwarica.com>
2019-05-20 12:06:29 +0545

    pic upload completed

--- app/src/main/java/com/e/heroapi/MainActivity.java
+++ app/src/main/java/com/e/heroapi/MainActivity.java
@@ -1,12 +1,22 @@
 package com.e.heroapi;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
+import android.support.v4.content.CursorLoader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -22,6 +32,42 @@ import url.Url;
 public class MainActivity extends AppCompatActivity {
     private EditText etName, etDescription;
     private Button btnSave;
+    private ImageView imgProfile;
+    String imagePath;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if (data == null) {
+                Toast.makeText(this, "please select an image", Toast.LENGTH_SHORT).show();
+            }
+        }
+        Uri uri  = data.getData();
+        imagePath = getRealPathFromUri(uri);
+        previewImage(imagePath);
+    }
+
+    private String getRealPathFromUri(Uri uri) {
+        String[] projection = {MediaStore.Images.Media.DATA};
+        CursorLoader loader = new CursorLoader(getApplicationContext(), uri, projection, null,
+                null, null);
+        Cursor cursor = loader.loadInBackground();
+        int colIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+        String result = cursor.getString(colIndex);
+        cursor.close();
+        return result;
+    }
+
+    private void previewImage(String imagePath) {
+        File imgfile = new File(imagePath);
+        if (imgfile.exists()){
+            Bitmap myBitmap = BitmapFactory.decodeFile(imgfile.getAbsolutePath());
+
+            imgProfile.setImageBitmap(myBitmap);
+        }
+   }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,8 +76,8 @@ public class MainActivity extends AppCompatActivity {
 
         etName = findViewById(R.id.etname);
         etDescription = findViewById(R.id.etDesc);
+        imgProfile = findViewById(R.id.imgProfile);
         btnSave = findViewById(R.id.btnSave);
-
         btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -39,100 +85,130 @@ public class MainActivity extends AppCompatActivity {
             }
 
             private void Save() {
-                String name = etName.getText().toString();
-                String desc = etDescription.getText().toString();
-
-                Map<String, String> map = new HashMap<>();
-                map.put("name",name);
-                map.put("desc",desc);
-
-                Retrofit retrofit = new Retrofit.Builder()
-                        .baseUrl(Url.BASE_URL)
-                        .addConverterFactory(GsonConverterFactory.create())
-                        .build();
-
-                HeroesAPI heroesAPI = retrofit.create(HeroesAPI.class);
-
-                Call<Void> heroesCall = heroesAPI.addHero(map);
-
-                heroesCall.enqueue(new Callback<Void>() {
-                    @Override
-                    public void onResponse(Call<Void> call, Response<Void> response) {
-                        if (!response.isSuccessful()) {
-                           Toast.makeText(MainActivity.this, " code " + response.code(), Toast.LENGTH_SHORT).show();
-                        }
-                        Toast.makeText(MainActivity.this, "Sucessfully Added", Toast.LENGTH_SHORT).show();
-                    }
-
-                    @Override
-                    public void onFailure(Call<Void> call, Throwable t) {
-                        Toast.makeText(MainActivity.this, "Error" + t.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
-                    }
-                });
-
             }
 
-//            private void Save() {
-//                String name = etName.getText().toString();
-//                String desc = etDescription.getText().toString();
-//
-//                Heroes heroes = new Heroes(name, desc);
-//                Retrofit retrofit = new Retrofit.Builder()
-//                        .baseUrl(Url.BASE_URL)
-//                        .addConverterFactory(GsonConverterFactory.create())
-//                        .build();
-//
-//                HeroesAPI heroesAPI = retrofit.create(HeroesAPI.class);
-//
-//                Call<Void> heroesCall = heroesAPI.addHero(name,desc);
-//
-//                heroesCall.enqueue(new Callback<Void>() {
-//                    @Override
-//                    public void onResponse(Call<Void> call, Response<Void> response) {
-//                        if (!response.isSuccessful()) {
-//                           Toast.makeText(MainActivity.this, " code " + response.code(), Toast.LENGTH_SHORT).show();
-//                        }
-//                        Toast.makeText(MainActivity.this, "Sucessfully Added", Toast.LENGTH_SHORT).show();
-//                    }
-//
-//                    @Override
-//                    public void onFailure(Call<Void> call, Throwable t) {
-//                        Toast.makeText(MainActivity.this, "Error" + t.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
-//
-//                    }
-//                });
-//            }
-
-//            private void Save() {
-//                String name = etName.getText().toString();
-//                String desc = etDescription.getText().toString();
-//
-//                Heroes heroes = new Heroes(name, desc);
-//                Retrofit retrofit = new Retrofit.Builder()
-//                        .baseUrl(Url.BASE_URL)
-//                        .addConverterFactory(GsonConverterFactory.create())
-//                        .build();
-//
-//                HeroesAPI heroesAPI = retrofit.create(HeroesAPI.class);
-//
-//                Call<Void> heroesCall = heroesAPI.addHero(heroes);
-//
-//                heroesCall.enqueue(new Callback<Void>() {
-//                    @Override
-//                    public void onResponse(Call<Void> call, Response<Void> response) {
-//                        if (!response.isSuccessful()) {
-//                           Toast.makeText(MainActivity.this, " code " + response.code(), Toast.LENGTH_SHORT).show();
-//                        }
-//                        Toast.makeText(MainActivity.this, "Sucessfully Added", Toast.LENGTH_SHORT).show();
-//                    }
-//
-//                    @Override
-//                    public void onFailure(Call<Void> call, Throwable t) {
-//                        Toast.makeText(MainActivity.this, "Error" + t.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
-//                    }
-//                });
-//
-//            }
         });
+
+
+        imgProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BrowseImage();
+            }
+
+            private void BrowseImage() {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent, 0);
+            }
+
+
+
+
+        });
+
+
+//        btnSave.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Save();
+//            }
+//
+//            private void Save() {
+//                String name = etName.getText().toString();
+//                String desc = etDescription.getText().toString();
+//
+//                Map<String, String> map = new HashMap<>();
+//                map.put("name",name);
+//                map.put("desc",desc);
+//
+//                Retrofit retrofit = new Retrofit.Builder()
+//                        .baseUrl(Url.BASE_URL)
+//                        .addConverterFactory(GsonConverterFactory.create())
+//                        .build();
+//
+//                HeroesAPI heroesAPI = retrofit.create(HeroesAPI.class);
+//
+//                Call<Void> heroesCall = heroesAPI.addHero(map);
+//
+//                heroesCall.enqueue(new Callback<Void>() {
+//                    @Override
+//                    public void onResponse(Call<Void> call, Response<Void> response) {
+//                        if (!response.isSuccessful()) {
+//                           Toast.makeText(MainActivity.this, " code " + response.code(), Toast.LENGTH_SHORT).show();
+//                        }
+//                        Toast.makeText(MainActivity.this, "Sucessfully Added", Toast.LENGTH_SHORT).show();
+//                    }
+//
+//                    @Override
+//                    public void onFailure(Call<Void> call, Throwable t) {
+//                        Toast.makeText(MainActivity.this, "Error" + t.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+//                    }
+//                });
+//
+//            }
+//
+////            private void Save() {
+////                String name = etName.getText().toString();
+////                String desc = etDescription.getText().toString();
+////
+////                Heroes heroes = new Heroes(name, desc);
+////                Retrofit retrofit = new Retrofit.Builder()
+////                        .baseUrl(Url.BASE_URL)
+////                        .addConverterFactory(GsonConverterFactory.create())
+////                        .build();
+////
+////                HeroesAPI heroesAPI = retrofit.create(HeroesAPI.class);
+////
+////                Call<Void> heroesCall = heroesAPI.addHero(name,desc);
+////
+////                heroesCall.enqueue(new Callback<Void>() {
+////                    @Override
+////                    public void onResponse(Call<Void> call, Response<Void> response) {
+////                        if (!response.isSuccessful()) {
+////                           Toast.makeText(MainActivity.this, " code " + response.code(), Toast.LENGTH_SHORT).show();
+////                        }
+////                        Toast.makeText(MainActivity.this, "Sucessfully Added", Toast.LENGTH_SHORT).show();
+////                    }
+////
+////                    @Override
+////                    public void onFailure(Call<Void> call, Throwable t) {
+////                        Toast.makeText(MainActivity.this, "Error" + t.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+////
+////                    }
+////                });
+////            }
+//
+////            private void Save() {
+////                String name = etName.getText().toString();
+////                String desc = etDescription.getText().toString();
+////
+////                Heroes heroes = new Heroes(name, desc);
+////                Retrofit retrofit = new Retrofit.Builder()
+////                        .baseUrl(Url.BASE_URL)
+////                        .addConverterFactory(GsonConverterFactory.create())
+////                        .build();
+////
+////                HeroesAPI heroesAPI = retrofit.create(HeroesAPI.class);
+////
+////                Call<Void> heroesCall = heroesAPI.addHero(heroes);
+////
+////                heroesCall.enqueue(new Callback<Void>() {
+////                    @Override
+////                    public void onResponse(Call<Void> call, Response<Void> response) {
+////                        if (!response.isSuccessful()) {
+////                           Toast.makeText(MainActivity.this, " code " + response.code(), Toast.LENGTH_SHORT).show();
+////                        }
+////                        Toast.makeText(MainActivity.this, "Sucessfully Added", Toast.LENGTH_SHORT).show();
+////                    }
+////
+////                    @Override
+////                    public void onFailure(Call<Void> call, Throwable t) {
+////                        Toast.makeText(MainActivity.this, "Error" + t.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+////                    }
+////                });
+////
+////            }
+//        });
     }
 }
